feb734c2c55ff7054865f1aa2b1ca4dff7af18ec
m-lavrovsky <mark_lv@mail.ru>
2022-01-17 04:40:20 +0300

    initial

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,16 +8,10 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Environment;
 
-import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class  Util {
-    private final String url;
-    private final String user;
-    private final String password;
-    private Connection dbConnection;
-
     private static SessionFactory sessionFactory;
     private static StandardServiceRegistry registry;
 
@@ -57,36 +51,4 @@ public class  Util {
 
     public static SessionFactory getSessionFactory() {return sessionFactory;}
 
-    // реализуйте настройку соеденения с БД
-    public Util (String url, String user, String password) {
-        this.url = url;
-        this.password = password;
-        this.user = user;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public String getPassword() { return password; }
-
-    /*public void connect() throws SQLException {
-        try {
-            System.out.println("Establishing DB connection...");
-            dbConnection = DriverManager.getConnection(url, username, password);
-            System.out.println("DB connection successful");
-        }
-        catch(SQLException ex) {
-            System.out.println("DB connection failed:");
-            System.out.println(ex);
-        }
-    }
-
-    public void close() {
-        dbConnection.close();
-    }*/
 }
